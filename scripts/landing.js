d0afd7c548321cdb5179189071c7bd36aeb7201f
Pankhil <pankhilmistry@gmail.com>
2018-01-21 16:42:31 +0530

    cp_23_Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,8 @@
-var animatePoints = function() {
- 
-                 var points = document.getElementsByClassName('point');
- 
+
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+
                  var revealPoint = function(index) {
                                 
                      points[index].style.opacity = 1;
@@ -13,6 +14,19 @@ var animatePoints = function() {
                 for (var i=0; i<points.length;i++){
                     revealPoint(i);
                 }
- 
-                
              };
+
+             window.onload=function(){
+
+                if (window.innerHeight > 950) {
+                    animatePoints(pointsArray);
+                    }
+
+                var sellingPoints=document.getElementsByClassName('selling-points')[0];
+                var scrollDistance=sellingPoints.getBoundingClientRect().top-window.innerHeight+200;
+                window.addEventListener('scroll', function(event){
+                    if(document.documentElement.scrollTop||document.body.scrollTop>=scrollDistance){
+                        animatePoints(pointsArray);
+                    }
+                });
+             }
