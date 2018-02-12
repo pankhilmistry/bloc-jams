95c02e192ab374c30ef9c2921c7c5e684c2adb62
Pankhil <pankhilmistry@gmail.com>
2018-02-12 10:35:41 +0530

    CP 32 jQuery NextPrev

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumNewton = {
+     title: 'Equations',
+     artist: 'Issac Newton',
+     label: 'Spinnin Records',
+     year: '1727',
+     albumArtUrl: 'assets/images/album_covers/14.png',
+     songs: [
+         { title: 'Gravity?', duration: '3:08' },
+         { title: 'My laws of motion', duration: '4:21' },
+         { title: 'Apple of my eye', duration: '3:25'},
+         { title: 'Reflections', duration: '6:05' },
+         { title: 'Physical cosmology', duration: '4:15'}
+     ]
+ };
